fa62fce60e1aea2901319b7b015db9bfdfa8bba4
Yurdiansyah <yurdi@griffin>
2022-10-18 09:57:15 +0700

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,26 @@
-import Button from "../components/button/Button";
-import Navigation from "../components/Navigation";
+import Icon from "../components/icon/Icon";
+import {AiOutlineSearch,AiOutlineBell} from 'react-icons/ai'
+import {BsChevronDown} from 'react-icons/bs'
 
 function Dashboard() {
     return (
-        <h1>aaa</h1>
-    )
+        <div className="bg-[#E5E5E5] h-screen">
+            <div className="py-5 border-b bg-white border-b-slate-300 flex justify-between">
+                <div className="flex justify-between mx-auto w-11/12">
+                    <h1 className="my-auto font-bold text-3xl">Dashboard</h1>
+                    <div className="flex justify-end gap-3 content-center text-center">
+                        <Icon icon={<AiOutlineSearch className="text-2xl" />} />
+                        <Icon icon={<AiOutlineBell className="text-2xl" />} />
+                        <Icon icon={<img src="https://www.wikihow.com/images/thumb/1/13/Stewie-step-Step-9.jpg/v4-460px-Stewie-step-Step-9.jpg.webp" className="object-cover w-full h-full rounded-full"/>} />
+                        <h1 className="content-center text-center my-auto text-base font-bold">yurdigrfnn</h1>
+                        <button className="px-2">
+                            <BsChevronDown className="" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
 }
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ import Sidebar from "../parts/sidebar/Sidebar";
 export default function Home() {
     return (
         <div className="flex w-full">
-            <div className="w-1/6">
+            <div className="lg:w-1/6 sm:w-5/12 md:w-4/12">
                 <Sidebar />
             </div>
-            <div className="w-5/6">
+            <div className="lg:w-5/6 sm:w-7/12 md:w-8/12">
                 <Outlet />
             </div>
         </div>
