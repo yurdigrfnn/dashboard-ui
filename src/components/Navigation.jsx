9261b7fbc73783a78ba3acb9e38439302da0b518
Yurdiansyah <yurdi@griffin>
2022-10-18 09:56:48 +0700

    dashboard

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,10 @@
-import { NavLink,useResolvedPath } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 export default function Navigation({to,icon,name}) {
-    const bgactive = "ml-4 pl-4 rounded-l-lg bg-[#1B2B65]";
+    const active = "ml-4 pl-4 rounded-l-lg bg-[#1B2B65]";
     const inActive = "ml-4 pl-4 rounded-l-lg hover:bg-[#152355]";
-
-
+    
     return (
-        <NavLink to={to} end className={({ isActive }) => isActive ? bgactive : inActive} >
+        <NavLink to={to} end className={({ isActive }) => isActive ? active : inActive} >
                 <div className="flex text-white py-[18px] gap-2 ">
                     {icon}
                     <h1 className='my-auto'>{name}</h1>
